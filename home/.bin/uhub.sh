8980dbc14ade8c2de5fd26196d1096e0e2981fa3
ChefIronBelly <werthmation@gmail.com>
2018-10-13 08:55:47 -0400

    adds and tweaks

--- home/.bin/uhub.sh
+++ home/.bin/uhub.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-if [ -d "/home/chef/Source/OOTB-defaults" ]; then
-cd /home/chef/Source/OOTB-defaults && echo "[Updating] Out of the Box ..." && git pull
-fi
-if [ -d "/home/chef/Source/BSD-Configs" ]; then
-cd /home/chef/Source/BSD-Configs && echo "[Updating] BSD-Configs ..." && git pull
+if [ -d "/home/chef/Source/NetBSD" ]; then
+cd /home/chef/Source/NetBSD && echo "[Updating] NetBSD ..." && git pull
 fi
 if [ -d "/home/chef/Source/core" ]; then
 cd /home/chef/Source/core && echo "[Updating] wmutils core ..." && git pull 
@@ -15,16 +12,10 @@ fi
 if [ -d "/home/chef/Source/libwm" ]; then
 cd /home/chef/Source/libwm && echo "[Updating] wmutils lib ..." && git pull
 fi
-if [ -d "/Source/x11fs " ]; then
-cd /home/chef/Source/x11fs && echo "[Updating] x11fs ..." && git pull 
-fi
 if [ -d "/home/chef/Source/bar" ]; then
 cd /home/chef/Source/bar && echo "[Updating] lemonbar ..." && git pull 
 fi
 if [ -d "/home/chef/Source/sxhkd" ]; then
 cd /home/chef/Source/sxhkd && echo "[Updating] shxkd ..." && git pull
 fi 
-if [ -d "/home/chef/Source/Base" ]; then
-cd /home/chef/Source/Base && echo "[Updating] Pur Linux base ..." && git pull
-fi
 echo "[All] githubs updated ..."
