9bb8e37d658eda5953ff7e10f8a051a27a6c961f
ChefIronBelly <werthmation@gmail.com>
2025-04-12 09:01:46 -0400

    adds and tweaks

--- home/Source/slstatus/config.h
+++ home/Source/slstatus/config.h
@@ -65,10 +65,10 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-  { cpu_perc, " [CPU:%s%%]", NULL},
-  { ram_used, " [RAM:%s/", NULL},
-  { ram_total, "%s]", NULL},
-  { vol_perc, " [VOL:%s%%]", "outputs.master"},
+  { cpu_perc, " ::Cpu:%s%%::", NULL},
+  { ram_used, " ::Ram:%s/", NULL},
+  { ram_total, "%s::", NULL},
+  { vol_perc, " ::Vol:%s%%::", "outputs.master"},
   /*{ battery_perc, " [BAT:%s%%]", NULL},*/
-  { datetime, "[%s]",           "%I:%M" },
+  { datetime, "%s",           "%I:%M" },
 };
