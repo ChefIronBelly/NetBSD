4445f219dfd73615c6dd4ccd227cd8a1d9b63e30
ChefIronBelly <werthmation@gmail.com>
2025-04-19 07:51:46 -0400

    adds and tweaks

--- home/.bin/gNetBSD.sh
+++ home/.bin/gNetBSD.sh
@@ -1,16 +1,24 @@
 #!/usr/pkg/bin/bash
 #
-# Get NetBSD daily sets
+# Get NetBSD-10.99.* sets
 #
 # depends: curl and common sense
 
 set -e
 set -x
 
-now=$(date +"%Y%m%d%I%M""Z")
+now=$(date +"%m%d%I%M""Z")
 mkdir /home/chef/HEAD/${now} && cd $_
+echo $now
 
-for file in kern-GENERIC modules base comp etc gpufw.tar.xz man text xbase xcomp xetc xfont xserver
+mkdir -p /home/chef/HEAD/${now}/binary/sets && cd $_
+for file in kern-GENERIC modules base comp etc gpufw man text xbase xcomp xetc xfont xserver
 do
-    curl -C - -LO http://nyftp.netbsd.org/pub/NetBSD-daily/HEAD/latest/amd64/binary/sets/${file}.tar.xz
+    curl -C - -LO http://nyftp.netbsd.org/pub/NetBSD-daily/HEAD/202504190100Z/amd64/binary/sets/${file}.tar.xz
 done
+
+cd ..
+mkdir -p /home/chef/HEAD/${now}/binary/kernel && cd $_
+curl -C - -LO http://nyftp.netbsd.org/pub/NetBSD-daily/HEAD/202504190100Z/amd64/binary/kernel/netbsd-GENERIC.gz
+
+# sysupgrade auto /HEAD/latest
